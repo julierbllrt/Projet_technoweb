9722c41ccce297104b3708ff9106f58760578c08
julierbllrt <julie.robillart@isen.yncrea.fr>
2017-10-13 16:12:52 +0200

    hide&show recherche

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function recherche() {
+    var x = document.getElementById("tout");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
