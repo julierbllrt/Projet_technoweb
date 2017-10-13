d932546536748433483c45336205027e11995c41
julierbllrt <julie.robillart@isen.yncrea.fr>
2017-10-13 16:41:32 +0200

    hide&show connexion

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
-function recherche() {
-    var x = document.getElementById("tout");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+function showRecherche() {
+	var x = document.getElementById("searchfield");
+	var y = document.getElementById("form");
+	if (x.style.display === "none") {
+		x.style.display = "flex";
+		y.style.display = "none";
+	} else {
+		x.style.display = "none";
+	}
+}
+
+function showConnexion() {
+	var x = document.getElementById("searchfield");
+	var y = document.getElementById("form");
+       if (y.style.display === "none") {
+		y.style.display = "block";
+		x.style.display = "none";
+	} else {
+		y.style.display = "none";
+	}
 }
