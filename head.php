8c7c6da7258a0990f1a8f0a6f47144e1f9a823b8
unknown <benoit.pepin@isen.yncrea.fr>
2017-11-07 16:33:27 +0100

    fix encore les problemes

--- head.php
+++ head.php
@@ -2,6 +2,6 @@
 <meta charset="UTF-8">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script src="js/index.js"/></script>
-<script src="js/main.js"/></script>
+
 <script src="js/panier.js"/></script>
 	
