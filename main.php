277d1294877024cd953fa4c07a6c4bc8616488e0
unknown <benoit.pepin@isen.yncrea.fr>
2017-11-10 16:07:21 +0100

    main en cours de structuration avec des variables

--- main.php
+++ main.php
@@ -1,5 +1,13 @@
 <!DOCTYPE html>
-<?php session_start (); ?>
+<?php session_start (); 
+
+$_currentAction = 'section' ;
+var_dump( $_GET);
+if(!empty($_GET['action_result'])){
+	$_currentAction = $_GET['action_result'] ;
+}
+echo $_currentAction;
+?>
 <html >
 
 	<head>
@@ -14,7 +22,7 @@
 		
 		
 		<section id = "page">
-			<?php include('section.php'); ?>	
+			<?php include($_currentAction .'.php'); ?>	
 			<?php include('carrousel.php');?>
 			<?php include('result.php');?>
 			
