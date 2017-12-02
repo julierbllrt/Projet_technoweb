649aab73ec8e3cacce8df6dc4378f16cd1b19154
unknown <benoit.pepin@isen.yncrea.fr>
2017-12-02 18:18:52 +0100

    ajout de deux pages

---
+++ search.php
@@ -0,0 +1,6 @@
+	<form class="searchfield" method="post" action="index.php?action_result=result">
+
+		<input id="barrederecherche" name="recherche"  value="montre" ></input>
+		<input  id="bouton_recherche" type="submit" value="Recherche" />
+
+	</form>
